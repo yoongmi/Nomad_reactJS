3cf88b9a847986f8fbd0e050ee9ffdaa186da755
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-09-15 01:28:55 +0900

    todolist 이동

--- todo-list/src/theme.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultTheme } from "styled-components";
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#fefefe",
-  accentColor: "#feca57",
-  cardBgColor: "rgba(0,0,0,0.45)",
-};
