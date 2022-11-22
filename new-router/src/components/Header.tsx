7d969bbff850d9eb76c0ec933c7b036c39e55bd0
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-11-22 21:29:05 +0900

    useNavigate, useParams

--- new-router/src/components/Header.tsx
+++ new-router/src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about");
+  };
   return (
     <header>
       <ul>
@@ -8,6 +12,7 @@ function Header() {
         </li>
         <li>
           <Link to={"/about"}>About</Link>
+          <button onClick={onAboutClick}>about</button>
         </li>
       </ul>
     </header>
--- new-router/src/screens/Home.tsx
+++ new-router/src/screens/Home.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import { users } from "../db";
+
 const Home = () => {
-  const users: any = [];
-  return <>{users[0].name}</>;
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default Home;
